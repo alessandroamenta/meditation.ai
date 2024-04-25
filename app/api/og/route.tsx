92f5a5a65b3a579c188293681fa12cefdc17a3b6
alessandroamenta <alessandroamenta1@gmail.com>
2024-04-25 18:31:00 +0200

    Update branding and site name

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -55,7 +55,7 @@ export async function GET(req: Request) {
               color: 'transparent'
             }}
           >
-            SaaS Starter
+            🧘‍♀️ Dora.AI
           </div>
 
           <div tw="flex flex-col flex-1 py-16">
--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,7 @@ import { SiteConfig } from "types"
 const site_url = env.NEXT_PUBLIC_APP_URL;
 
 export const siteConfig: SiteConfig = {
-  name: "SaaS Starter",
+  name: "🧘‍♀️Dora.AI",
   description:
     "Get your project off to an explosive start with SaaS Starter! Harness the power of Next.js 14, Prisma, Planetscale, Auth.js v5, Resend, React Email, Shadcn/ui and Stripe to build your next big thing.",
   url: site_url,
