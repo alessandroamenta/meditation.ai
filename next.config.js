cd4756bd22bbe42489c457c60e41daaf416185ef
alessandroamenta <alessandroamenta1@gmail.com>
2024-05-03 13:47:33 +0200

    Add transpilePackages option to next.config.js

--- next.config.js
+++ next.config.js
@@ -24,6 +24,7 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client"],
   },
+  transpilePackages: ['fluent-ffmpeg'],
 }
 
 const withContentlayer = createContentlayerPlugin({
