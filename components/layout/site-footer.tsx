6fb34eafa71b769e8014d32378e73f3bdc3b1d95
alessandroamenta <alessandroamenta1@gmail.com>
2024-05-03 10:41:45 +0200

    Update site footer text

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -12,16 +12,16 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose md:text-left">
-            Built by{" "}
+            Made by a solo indie maker. Follow me on {" "}
             <a
               href={siteConfig.links.twitter}
               target="_blank"
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              ale
+              X
             </a>
-            . The code is available on{" "}
+            . The code is available on {" "}
             <a
               href={siteConfig.links.github}
               target="_blank"
