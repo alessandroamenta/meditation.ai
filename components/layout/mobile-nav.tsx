af6c2457316bf3b70f09c462783df37dc34341cb
alessandroamenta <alessandroamenta1@gmail.com>
2024-05-10 13:27:29 +0200

    /

--- components/layout/mobile-nav.tsx
+++ components/layout/mobile-nav.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Link from "next/link";
-
 import { MainNavItem } from "types";
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
