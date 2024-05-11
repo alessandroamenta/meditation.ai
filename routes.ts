dae2c617313dde3462d681712497757e1e887e0a
alessandroamenta <alessandroamenta1@gmail.com>
2024-05-11 15:45:39 +0200

    Add privacy route to publicRoutes

--- routes.ts
+++ routes.ts
@@ -3,7 +3,7 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ["/", "/blog", "/docs", "/guides", "/pricing"];
+export const publicRoutes = ["/", "/blog", "/docs", "/guides", "/pricing", "/privacy"];
 
 /**
  * An array of routes that are used for authentication
